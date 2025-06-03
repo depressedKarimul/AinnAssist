09c263ba52c85dd56b3558ffd7c919c7229531e4
karimul-hassan <karimul.hassan09@gmail.com>
2025-06-03 20:44:51 +0600

    Create Chunks

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
-# Step 1: Load raw PDF(s)
+# Step 1:clear(s)
 DATA_PATH="data/"
 def load_pdf_files(data):
     loader = DirectoryLoader(data,
@@ -14,3 +14,15 @@ def load_pdf_files(data):
 
 documents=load_pdf_files(data=DATA_PATH)
 print("Length of PDF pages: ", len(documents))
+
+
+
+# Step 2: Create Chunks
+def create_chunks(extracted_data):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500,
+                                                 chunk_overlap=50)
+    text_chunks=text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+text_chunks=create_chunks(extracted_data=documents)
+print("Length of Text Chunks: ", len(text_chunks))
